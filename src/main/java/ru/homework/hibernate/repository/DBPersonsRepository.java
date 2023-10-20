a78ebbca7184ac64c67cf41a679f3a41e808620b
Nikita Novikov <monstars51@gmail.com>
2023-10-20 23:24:17 +0300

    add @Query

--- src/main/java/ru/homework/hibernate/repository/DBPersonsRepository.java
+++ src/main/java/ru/homework/hibernate/repository/DBPersonsRepository.java
@@ -1,6 +1,8 @@
 package ru.homework.hibernate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.homework.hibernate.entity.PersonId;
 import ru.homework.hibernate.entity.Persons;
@@ -9,9 +11,12 @@ import java.util.List;
 
 @Repository
 public interface DBPersonsRepository extends JpaRepository<Persons, PersonId> {
-    List<Persons> findPersonByCityOfLiving(String city);
+    @Query("select p from Persons p where p.cityOfLiving = :cityOfLiving")
+    List<Persons> findPersonByCityOfLiving(@Param("cityOfLiving")String cityOfLiving);
 
-    List<Persons> findPersonsByPersonIdAgeLessThanOrderByPersonIdAge(int age);
+    @Query("select p from Persons p where p.personId.age < :age order by p.personId.age")
+    List<Persons> findPersonsByPersonIdAgeLessThanOrderByPersonIdAge(@Param("age")int age);
 
+    @Query("select p from Persons p where p.personId.name = :name and p.personId.surname = :surname")
     List<Persons> findByPersonIdNameAndPersonIdSurname(String name, String surname);
 }
